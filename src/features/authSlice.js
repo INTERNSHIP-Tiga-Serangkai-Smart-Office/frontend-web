25361667bec608cec3f4b41e337e0aa4a85099bd
sadewaammar21 <sadewaammars47t@gmail.com>
2024-04-22 11:08:02 +0700

    Feat-API
    menambahkan logic API untuk login

---
+++ src/features/authSlice.js
@@ -0,0 +1,52 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+    user: null,
+    isEror: false,
+    isSuccess: false,
+    isLoading: false,
+    message:""
+}
+
+export const Login = createAsyncThunk("user/LoginUser", async(user,thunkAPI)=>{
+    try {
+        const response = await axios.post('http://localhost:5000/login',{
+            email: user.email,
+            password: user.password
+        });
+        return response.data;
+    } catch (error) {
+        if(error.response){
+            const message = error.response.data.msg;
+            return thunkAPI.rejectWithValue(message);
+        }
+    }
+});
+
+export const authSlice = createSlice({
+    name:"auth",
+    initialState,
+    reducers:{
+        reset: (state) => initialState
+    },
+    extraReducers:(builder)=>{
+        builder.addCase(Login.pending, (state)=>{
+            state.isLoading = true;
+        });
+        builder.addCase(Login.fulfilled,(state, action)=>{
+            state.isLoading = false;
+            state.isSuccess = true;
+            state.user= action.payload;
+        });
+        builder.addCase(Login.rejected, (state, action)=>{
+            state.isLoading = false;
+            state.isEror = true;
+            state.message = action.payload;
+        });
+
+    }
+});
+
+export const {reset} = authSlice.actions;
+export default authSlice.reducer;
