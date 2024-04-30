60e99707483302faebe761d187d7fab92e603a84
Hernawan Abidin <hernaone1245@gmail.com>
2024-04-30 11:08:34 +0700

    dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,7 +3,13 @@ import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import {BsArrowLeftShort,} from "react-icons/bs";
 import {AiFillEnvironment} from "react-icons/ai";
-import Sidebar from '../components/Sidebar'
+import { RiDashboardFill } from "react-icons/ri";
+import { BiSolidEditLocation } from "react-icons/bi";
+import { FaFolder } from "react-icons/fa";
+import { LuFolderInput } from "react-icons/lu";
+import { LuFolderOutput } from "react-icons/lu";
+import { TbReportAnalytics } from "react-icons/tb";
+import SideBar from '../components/SideBar'
 
 const Dashboard = () => {
   const [open, setOpen] = useState(true);
@@ -22,18 +28,30 @@ const Dashboard = () => {
         </div>
         
         <ul className='pt-5'>
-                  <li className='text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white 
-                  rounded-md mt-2 text-white font-montserrat font-semibold'><NavLink>Dashboard</NavLink></li>
-                  <li className='text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white 
-                  rounded-md mt-2 text-white font-montserrat font-semibold'><NavLink>Data Asset</NavLink></li>
-                  <li className='text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white 
-                  rounded-md mt-2 text-white font-montserrat font-semibold'><NavLink>Relokasi</NavLink></li>
-                  <li className='text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white 
-                  rounded-md mt-2 text-white font-montserrat font-semibold'><NavLink>Asset Masuk</NavLink></li>
-                  <li className='text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white 
-                  rounded-md mt-2 text-white font-montserrat font-semibold'><NavLink>Asset Keluar</NavLink></li>
-                  <li className='text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white 
-                  rounded-md mt-2 text-white font-montserrat font-semibold'><NavLink>Laporan</NavLink></li>
+                  <li className='text-sm flex flex-row items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white rounded-md mt-2 text-white font-montserrat font-semibold'>
+                    <RiDashboardFill className='w-6 h-6'/> 
+                    {open && <NavLink>Dashboard</NavLink>}
+                  </li>
+                  <li className='text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white  rounded-md mt-2 text-white font-montserrat font-semibold'>
+                    <FaFolder className='w-6 h-6'/>
+                    {open && <NavLink>Data Asset</NavLink>}
+                  </li>
+                  <li className='text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white rounded-md mt-2 text-white font-montserrat font-semibold'>
+                    <BiSolidEditLocation className='w-6 h-6'/>
+                    {open && <NavLink>Relokasi</NavLink>}
+                  </li>
+                  <li className='text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white rounded-md mt-2 text-white font-montserrat font-semibold'>
+                    <LuFolderInput className='w-6 h-6'/>
+                    {open && <NavLink>Asset Masuk</NavLink>}
+                  </li>
+                  <li className='text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white rounded-md mt-2 text-white font-montserrat font-semibold'>
+                    <LuFolderOutput className='w-6 h-6'/>
+                    {open && <NavLink>Asset Keluar</NavLink>}
+                  </li>
+                  <li className='text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white rounded-md mt-2 text-white font-montserrat font-semibold'>
+                    <TbReportAnalytics className='w-6 h-6'/>
+                    {open && <NavLink>Laporan</NavLink>}
+                  </li>
                   
                 </ul>
                 
