5a5fdc33367c1bff8f14bac006b2602938fb3071
Hernawan Abidin <hernaone1245@gmail.com>
2024-04-30 11:08:19 +0700

    icon on dashboard

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 
-function Sidebar({}) {
+const SideBar = () => {
   return (
-    <aside className='h-screen'>
-        <nav className='h-full flex flex-col bg-white birder-r shadow-sm'>
-            <div className='p-4 pb-2 flex justify-between items-center'>
-                <img src="https://img.logoipsum.com/243.svg" alt="" />
-            </div>
-        </nav>
-    </aside>
+    <div>
+      
+    </div>
   )
 }
 
-export default Sidebar
+export default SideBar
