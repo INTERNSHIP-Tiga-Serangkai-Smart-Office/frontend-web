1f82d8167ffa306475a5181a7e9bd31b303a846e
Hernawan Abidin <hernaone1245@gmail.com>
2024-05-15 08:46:13 +0700

    331

--- src/pages/DataAset.jsx
+++ src/pages/DataAset.jsx
@@ -1,6 +1,6 @@
 import Layout from '../components/Layout'
 import DataAsset from '../components/DataAsset'
-import SideBar from '../components/SideBar'
+import SideBar from '../components/Sidebar'
 import React from 'react'
 
 const DataAset = () => {
