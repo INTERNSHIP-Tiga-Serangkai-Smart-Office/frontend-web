0a5d4c18b346bddaa8a72e94e184d6582a2d9e41
Hernawan Abidin <hernaone1245@gmail.com>
2024-05-17 10:06:02 +0700

    sidebar toggle

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SideBar from './SideBar'
 
 const Layout = ({children}) => {
+  const [sidebarToggle, setSidebarToggle] = useState(true);
+
   return (
     <React.Fragment>
-        <div className='w-full h-full flex flex-row'>
-            <SideBar/>
-            <div className='p-10'>
-                <main>{children}</main>
+        <div className='flex'>
+            <SideBar sidebarToggle={sidebarToggle} setSidebarToggle={setSidebarToggle}/>
+            <div >
+                <div className={` ${sidebarToggle ? 'ml-72' : 'ml-20'} w-full p-10`}>{children}</div>
             </div>
 
         </div>
